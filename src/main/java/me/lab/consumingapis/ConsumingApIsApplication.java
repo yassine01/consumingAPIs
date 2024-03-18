a846a126a22acf8cf16f905339b4a9b592c871a2
Yass <yassine.mjardi@gmail.com>
2024-03-18 17:11:04 +0100

    :fire: Init & consuming (SOAP & REST) APIs.

---
+++ src/main/java/me/lab/consumingapis/ConsumingApIsApplication.java
@@ -0,0 +1,13 @@
+package me.lab.consumingapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingApIsApplication.class, args);
+    }
+
+}
